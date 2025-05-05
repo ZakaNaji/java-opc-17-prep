41c3358148f8de49cc0f50e7bbaf1138fcbb800b
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-05-05 18:35:33 +0100

    chapter 5: java passes by value - in case of a mutable ref type, passing by value will change the original ref, bc bouth it and the methode param point to the same object

--- src/main/java/com/znaji/method/passingData/MyClass.java
+++ src/main/java/com/znaji/method/passingData/MyClass.java
@@ -3,12 +3,14 @@ package com.znaji.method.passingData;
 public class MyClass {
 
     public static void main(String[] args) {
-        String name = new String("Zakaria");
+        StringBuilder name = new StringBuilder("Zakaria");
         changeName(name);
         System.out.println(name);
     }
 
-    private static void changeName(String name) {
-        name += "Naji";
+    private static void changeName(StringBuilder name) {
+        name.append(" Naji");
     }
+
+
 }
