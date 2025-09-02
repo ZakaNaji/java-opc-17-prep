06d8281a4a89dea6d84f4a4bf32592fad847fbcd
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-09-02 14:59:30 +0100

    chapter 8: Consumer and BiConsumer are used to take a arg(or 2) and do an opperation(no return, void)

---
+++ src/main/java/com/znaji/chapter8/builtin/functions/ConsumerTest.java
@@ -0,0 +1,20 @@
+package com.znaji.chapter8.builtin.functions;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class ConsumerTest {
+    public static void main(String[] args) {
+        Consumer<String> printer = System.out::println;
+        printer.accept("TESTING A CONSUMER");
+
+        Map<String, Integer> map = new HashMap<>();
+        BiConsumer<String, Integer> m1 = map::put;
+        m1.accept("Bannana", 10);
+        m1.accept("Orange", 12);
+        m1.accept("Apple", 15);
+        System.out.println(map);
+    }
+}
